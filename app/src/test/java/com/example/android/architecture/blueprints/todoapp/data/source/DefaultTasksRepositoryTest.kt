8c31b7731f37e87da2ae64efeb408d5322c6dc8e
Бекзадбек Насирахунов <n.bekzad@gmail.com>
2021-08-06 09:05:16 +0600

    Finished fake datasources

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/DefaultTasksRepositoryTest.kt
@@ -0,0 +1,49 @@
+package com.example.android.architecture.blueprints.todoapp.data.source
+
+import com.example.android.architecture.blueprints.todoapp.data.Result
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Test
+
+class DefaultTasksRepositoryTest {
+    private val task1 = Task("task1", "description1")
+    private val task2 = Task("task2", "description2")
+    private val task3 = Task("task3", "description3")
+
+    private val remoteTasks = listOf(task1, task2).sortedBy { it.id }
+    private val localTasks = listOf(task3).sortedBy { it.id }
+    private val newTasks = listOf(task3).sortedBy { it.id }
+
+    private lateinit var tasksRemoteDataSource: FakeDataSource
+    private lateinit var tasksLocalDataSource: FakeDataSource
+
+    // Class under test
+    private lateinit var tasksRepository: DefaultTasksRepository
+
+    @Before
+    fun createRepository() {
+        tasksRemoteDataSource = FakeDataSource(remoteTasks.toMutableList())
+        tasksLocalDataSource = FakeDataSource(localTasks.toMutableList())
+
+        tasksRepository = DefaultTasksRepository(tasksRemoteDataSource, tasksLocalDataSource,
+                            Dispatchers.Unconfined)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun getTasks_requestAllTasksFromRemoteDataSource() = runBlockingTest {
+
+        // When tasks are requested from the tasks repository
+        val tasks = tasksRepository.getTasks(true) as Result.Success
+
+        // Then tasks are loaded from the remote data source
+        assertThat(tasks.data, `is`(remoteTasks))
+    }
+
+
+}
